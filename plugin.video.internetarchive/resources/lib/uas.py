0f61b01eff1ffb7e7b3d8cbaf0c983e8518e6189
03stevensmi <mankymania12@gmail.com>
2023-02-03 17:44:44 +0000

    Update uas.py

--- plugin.video.internetarchive/resources/lib/uas.py
+++ plugin.video.internetarchive/resources/lib/uas.py
@@ -1,7 +1,6 @@
 import random
 
 uas = [
-    'Mozilla/5.0 (Linux; Android 11; Pixel 5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.111 Mobile Safari/537.36',
     'Mozilla/5.0 (iPhone; CPU iPhone OS 14_7_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) FxiOS/36.0  Mobile/15E148 Safari/605.1.15',
     'Mozilla/5.0 (Linux; Android 9; KFTRWI) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.88 Safari/537.36',
     'Mozilla/5.0 (X11; Linux x86_64; rv:93.0) Gecko/20100101 Firefox/93.0',
